db89c706ab64b2cb31694f57329b4a55c61c10d5
Timothy Lai <timothylai1121@gmail.com>
2023-04-15 21:04:15 -0400

    adding commentController.js

---
+++ controllers/commentController.js
@@ -0,0 +1,30 @@
+const db = require('../models');
+
+module.exports = {
+    findAll: function(req, res) { // findAll is a method that finds all comments //
+        db.Comment.findAll({})
+        .then (dbComment => res.json(dbComment))
+        .catch(err => res.status(422).json(err));
+    },
+    findById: function(req, res) { // findById is a method that finds a comment by its id //
+        db.Comment.fineOne({ where: { id: req.params.id } })
+        .then (dbComment => res.json(dbComment))
+        .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) { // create is a method that creates a new comment //
+        db.Comment.create(req.body)
+        .then(dbComment => res.json(dbComment))
+        .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) { // update is a method that updates a comment by its id //
+        db.Comment.update(req, body, { where : { id: req.params.id } })
+        .then(dbComment => res.json(dbComment))
+        .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) { // remove is a method that removes a comment by its id //
+        db.Comment.destroy({ where: { id: req.params.id } })
+        .then(dbComment => res.json(dbComment))
+        .catch(err => res.status(422).json(err));
+    }
+};
+
--- controllers/postController.js
+++ controllers/postController.js
@@ -1,28 +1,30 @@
 // const db = require('../models');
 const db = require('../models');
 
+
+
 module.exports = {
-    findAll: function(req, res) {
+    findAll: function(req, res) { // findAll is a method that finds all posts //
         db.Post.findAll({})
         .then (dbPost => res.json(dbPost))
         .catch(err => res.status(422).json(err));
     },
-    findById: function(req, res) {
+    findById: function(req, res) { // findById is a method that finds a post by its id //
         db.Post.fineOne({ where: { id: req.params.id } })
         .then (dbPost => res.json(dbPost))
         .catch(err => res.status(422).json(err));
     },
-    create: function(req, res) {
+    create: function(req, res) { // create is a method that creates a new post //
         db.Post.create(req.body)
         .then(dbPost => res.json(dbPost))
         .catch(err => res.status(422).json(err));
     },
-    update: function(req, res) {
+    update: function(req, res) { // update is a method that updates a post by its id //
         db.Post.update(req, body, { where : { id: req.params.id } })
         .then(dbPost => res.json(dbPost))
         .catch(err => res.status(422).json(err));
     },
-    remove: function(req, res) {
+    remove: function(req, res) { // remove is a method that removes a post by its id //
         db.Post.destroy({ where: { id: req.params.id } })
         .then(dbPost => res.json(dbPost))
         .catch(err => res.status(422).json(err));
